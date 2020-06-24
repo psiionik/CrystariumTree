43696a3040c663ac99b26f9ad66548aa32f0fc60
bkbranden <bk2dh@virginia.edu>
2020-06-24 14:38:25 -0400

    Added the onclick logic to add X and O to the tic tac toe board (added turn text)

--- webfrontend/src/Containers/TicTacToe/Tictac.tsx
+++ webfrontend/src/Containers/TicTacToe/Tictac.tsx
@@ -23,7 +23,7 @@ export default class Tictac extends React.Component<{}, TictacState> {
     handleClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>, row_index: number, col_index: number){
         let mutable_board = this.state.board
         if (this.state.playerOneTurn) {
-            if (mutable_board[row_index][col_index] == "-"){
+            if (mutable_board[row_index][col_index] === "-"){
                 mutable_board[row_index][col_index] = "X"
                 this.setState({
                     board: mutable_board,
@@ -32,7 +32,7 @@ export default class Tictac extends React.Component<{}, TictacState> {
             }
         }
         else {
-            if (mutable_board[row_index][col_index] == "-"){
+            if (mutable_board[row_index][col_index] === "-"){
                 mutable_board[row_index][col_index] = "O"
                 this.setState({
                     board: mutable_board,
@@ -44,9 +44,11 @@ export default class Tictac extends React.Component<{}, TictacState> {
     }
 
     render() {
-
         return (
-            <Board board={this.state.board} clickFunction={this.handleClick} />
+            <div>
+                {this.state.playerOneTurn ? <h1>Player 1's Turn</h1> : <h1>Player 2's Turn</h1>}
+                <Board board={this.state.board} clickFunction={this.handleClick} />
+            </div>
         )
     }
 }
